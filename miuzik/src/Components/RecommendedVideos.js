e825b4ad5840244034790d359f46b4e2801aebde
Prateek Sharma <prateek.sharma1099@gmail.com>
2021-03-30 23:51:07 +0530

    RecommendedVideos Component build to help sidebar

---
+++ miuzik/src/Components/RecommendedVideos.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function RecommendedVideos() {
+    return (
+        <div>
+            <h1>This is where recommended videos go</h1>
+        </div>
+    )
+}
+
+export default RecommendedVideos
