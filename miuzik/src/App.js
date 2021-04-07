e64d4575b470f861e27c7e106da1803a79e6aaea
Bhanu pratap singh <pbhanu314@gmail.com>
2021-04-07 14:36:32 +0530

    theme file Added

--- miuzik/src/App.js
+++ miuzik/src/App.js
@@ -1,13 +1,10 @@
-import AppBar from './Components/AppBar';
 import Header from './Components/Header';
 
 function App() {
   return (
     <div className="App">
       <Header />
-      {/* <Sidebar></Sidebar> */}
-      {/* <recommendedVideos></recommendedVideos> */}
-      {/* <AppBar></AppBar> */}
+
     </div>
   );
 }
---
+++ miuzik/src/Theme/defaultTheme.js
@@ -0,0 +1,15 @@
+import { createMuiTheme } from "@material-ui/core"
+
+const theme = createMuiTheme({
+    palette: {
+        type: "dark",
+        primary: {
+            main: '#000'
+        },
+        secondary: {
+            main: '#131313'
+        }
+    }
+
+})
+export default theme;
