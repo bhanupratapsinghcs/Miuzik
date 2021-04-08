49bc13069ba7d0b1b07a320f58c7d7cb0d000ca0
Prateek Sharma <prateek.sharma1099@gmail.com>
2021-04-08 23:01:20 +0530

    testing for sidebar appearance

---
+++ miuzik/src/Components/Sidebar.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import './Sidebar.css';
+
+function Sidebar() {
+    return (
+        <div className='sidebar' >
+
+            <h1>this is where side bar will go</h1>
+        </div>
+    )
+
+}
+
+export default Sidebar
