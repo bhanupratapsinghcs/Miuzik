043430029f1d6c4702856a67e6f1f441481dc9b7
Prateek Sharma <prateek.sharma1099@gmail.com>
2021-04-12 00:28:03 +0530

    function added to the header list and input bar

--- miuzik/src/Components/Header.js
+++ miuzik/src/Components/Header.js
@@ -1,23 +1,46 @@
-import React from 'react'
+import React ,{useState} from 'react'
 import './Header.css';
-import MenuIcon from '@material-ui/icons/Menu';
 import SearchIcon from '@material-ui/icons/Search';
 import Avatar from '@material-ui/core/Avatar';
 import MoreVertIcon from '@material-ui/icons/MoreVert';
+import KeyboardBackspaceIcon from '@material-ui/icons/KeyboardBackspace';
 
 function Header() {
+
+// // const [btn, btnhandler]= useState('header_inputButton')
+
+function handleClick() {
+     const list = document.getElementsByClassName('header_list');
+     list[0].style.display= 'none';
+     const input = document.getElementsByClassName('header_input');
+     input[0].style.display = 'flex';
+}
+
+function backToList() {
+    const list = document.getElementsByClassName('header_list');
+    list[0].style.display= 'flex';
+    const input = document.getElementsByClassName('header_input');
+    input[0].style.display = 'none';
+}
+
     return (
         <div className='header'>
             <div className="header_left">
-                <MenuIcon className='menuIcon' />
                 <img className='header-logo' src="https://upload.wikimedia.org/wikipedia/commons/1/1b/Youtube_Music_logo.svg" alt="Header Youtube logo"/>
                 <h1 className='header-logo-text'>Music</h1>
             </div>
             
             <div className="header_input">
+                <KeyboardBackspaceIcon onClick={backToList} className='header_inputButton' />
                 <input type="text" placeholder="Search" />
-                <SearchIcon className='header_inputButton' />
             </div>
+
+            <ul className='header_list'>
+                <li><a className='anchor' >Home</a></li>
+                <li><a className='anchor' >Explore</a></li>
+                <li><a className='anchor' >Library</a></li>
+                <li><a onClick={handleClick} className='anchor' ><SearchIcon className='search-icon'/>Search</a></li>
+            </ul>
             
             <div className="header_icons">
                 <MoreVertIcon className='threeDots' />
