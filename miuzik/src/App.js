62e8f6b2b9e83eaa25fa9a55b26548adf7e9916e
Prateek Sharma <prateek.sharma1099@gmail.com>
2021-04-12 00:32:36 +0530

    unnecessary import statement and components

--- miuzik/src/App.js
+++ miuzik/src/App.js
@@ -1,17 +1,10 @@
 import Header from './Components/Header';
-import Sidebar from './Components/Sidebar';
-import RecommendedVideos from './Components/RecommendedVideos';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
       <Header />
-
-      <div className="app_page">
-        <Sidebar />
-        <RecommendedVideos />
-      </div>
     </div>
   );
 }
