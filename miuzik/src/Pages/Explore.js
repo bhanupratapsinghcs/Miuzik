ff66b39ba0c9a05297a0254e0a964c35e40f2704
Prateek Sharma <prateek.sharma1099@gmail.com>
2021-04-13 00:18:36 +0530

    Explore.js Page created to implement the Explore layout

---
+++ miuzik/src/Pages/Explore.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function Explore() {
+    return (
+        <div>
+            <h3 style={{color: 'white'}}>Explore</h3>
+        </div>
+    )
+}
+
+export default Explore
