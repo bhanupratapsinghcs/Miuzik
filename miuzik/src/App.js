0d54b0f6dae08162bb31ec1139f54c997119f3b6
Prateek Sharma <prateek.sharma1099@gmail.com>
2021-04-13 00:11:57 +0530

    Routing is done

--- miuzik/src/App.js
+++ miuzik/src/App.js
@@ -1,11 +1,23 @@
 import Header from './Components/Header';
 import './App.css';
+import {BrowserRouter as Router, Route, Switch} from 'react-router-dom'
+import Home from './Pages/Home';
+import Explore from './Pages/Explore';
+import Library from './Pages/Library';
+
 
 function App() {
   return (
-    <div className="App">
-      <Header />
-    </div>
+    <Router>
+      <div className="App">
+        <Header />
+        <Switch>
+          <Route path='/Pages/Home' exact component={Home}></Route>
+          <Route path='/Pages/Explore' component={Explore}></Route>
+          <Route path='/Pages/Library' component={Library}></Route>
+        </Switch>
+      </div>
+    </Router>
   );
 }
 
