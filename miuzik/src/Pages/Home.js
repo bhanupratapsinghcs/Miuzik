f46a826f4146623c3113f8b7c7eb2948ceecc074
Prateek Sharma <prateek.sharma1099@gmail.com>
2021-04-13 00:19:25 +0530

    Home.js For home implementation and just checking the scrolling effect

---
+++ miuzik/src/Pages/Home.js
@@ -0,0 +1,31 @@
+import React from 'react'
+
+function Home() {
+    return (
+        <div style={{backgroundColor:'black'}}>
+            <h3 style={{color: 'white'}}>Home</h3>
+            <h1>Home</h1>
+            <h1>Home</h1>
+            <h1>Home</h1>
+            <h1>Home</h1>
+            <h1>Home</h1>
+            <h1>Home</h1>
+            <h1>Home</h1>
+            <h1>Home</h1>
+            <h1>Home</h1>
+            <h1>Home</h1>
+            <h1>Home</h1>
+            <h1>Home</h1>
+            <h1>Home</h1>
+            <h1>Home</h1>
+            <h1>Home</h1>
+            <h1>Home</h1>
+            <h1>Home</h1>
+            <h1>Home</h1>
+            <h1>Home</h1>
+            <h1>Home</h1>
+        </div>
+    )
+}
+
+export default Home
