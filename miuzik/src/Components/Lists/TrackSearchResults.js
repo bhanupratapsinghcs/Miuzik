0b0f758171598eca44769c0a0c47e87da7892358
Prateek Sharma <prateek.sharma1099@gmail.com>
2021-04-25 20:48:09 +0530

    Structure to have search list : added + import statement for css file is also added

---
+++ miuzik/src/Components/Lists/TrackSearchResults.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import './TrackSearchResults.css'
+
+export default function TrackSearchResults({ track }) {
+    return (
+        <div className="container">
+            <img src={track.albumUrl} style={{height: "64px", width: "64px"}} />
+            <div className="innerRight">
+                <div className="title">{track.title}</div>
+            </div>
+        </div>
+    )
+}
