f5c5434d3c0703042def9808fed5b180c30e4776
Prateek Sharma <prateek.sharma1099@gmail.com>
2021-04-25 21:23:51 +0530

    Cursor property added + OnClick function added for player for future use

--- miuzik/src/Components/Lists/TrackSearchResults.js
+++ miuzik/src/Components/Lists/TrackSearchResults.js
@@ -3,7 +3,7 @@ import './TrackSearchResults.css'
 
 export default function TrackSearchResults({ track }) {
     return (
-        <div className="container">
+        <div className="container" style={{ cursor: "pointer" }} onClick={handlePlay} >
             <img src={track.albumUrl} style={{height: "64px", width: "64px"}} />
             <div className="innerRight">
                 <div className="title">{track.title}</div>
