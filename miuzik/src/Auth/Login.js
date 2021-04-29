4254bfa5b2948b4bf84ab943cc58d907eeff9a62
Bhanu pratap singh <pbhanu314@gmail.com>
2021-04-29 11:03:12 +0530

    client id change

--- miuzik/src/Auth/Login.js
+++ miuzik/src/Auth/Login.js
@@ -1,10 +1,9 @@
 import React from 'react'
 import Button from '@material-ui/core/Button'
 
-const AUTH_URL = "https://accounts.spotify.com/authorize?client_id=process.env.REACT_APP_CLIENT_ID&response_type=code&redirect_uri=http://localhost:3000&scope=streaming%20user-read-email%20user-read-private%20user-library-read%20user-library-modify%20user-read-playback-state%20user-modify-playback-state%20user-library-read%20user-read-recently-played"
+const AUTH_URL = "https://accounts.spotify.com/authorize?client_id=" + process.env.REACT_APP_CLIENT_ID + "&response_type=code&redirect_uri=http://localhost:3000&scope=streaming%20user-read-email%20user-read-private%20user-library-read%20user-library-modify%20user-read-playback-state%20user-modify-playback-state%20user-library-read%20user-read-recently-played"
 
 export default function Login() {
-    console.log("login")
     return (
         <div>
             <Button href={AUTH_URL} variant="contained">Login
